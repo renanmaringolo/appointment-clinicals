a06eaf2960dc0b159700f363f3d9b1635ba8c998
Renan Proença <renan.maringolo@gmail.com>
2021-06-10 20:07:39 -0300

    add table Patients

---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Patient, type: :model do
+  describe "validations" do 
+    subject { create(:patient) }
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:birth_date) }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_length_of(:cpf).is_equal_to(11) }
+    it { should validate_length_of(:name).is_at_most(130) }
+  end
+end
